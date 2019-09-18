a57b7671fdc9420c4600c74ae03e827e40cfdc9a
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2019-09-18 17:52:44 +0300

    Koikatu working

---
+++ CardOrganizer/Arguments.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+
+namespace CardOrganizer
+{
+    internal class Arguments
+    {
+        [Value(0, MetaName = "targetfolder", Required = true, HelpText = "The cards in this folder will organized")]
+        public string TargetFolder { get; set; }
+
+        [Option("subfolder", Default = false, Required = false, HelpText = "Seach subfolders for cards")]
+        public bool SearchSubfolders { get; set; }
+
+        [Option("testrun", Default = false, Required = false, HelpText = "Test the program without moving files")]
+        public bool TestRun { get; set; }
+    }
+}
---
+++ CardOrganizer/CardConstants.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace CardOrganizer
+{
+    internal static class CardConstants
+    {
+        public const string SceneCategory = "scene";
+        public const string FemaleCategory = "female";
+        public const string MaleCategory = "male";
+        public const string OutfitCategory = "outfit";
+
+        public static readonly byte[] KoikatuStudioToken = Encoding.UTF8.GetBytes("【KStudio】");
+        public static readonly byte[] KoikatuCharaToken = Encoding.UTF8.GetBytes("【KoiKatuChara");
+        public static readonly byte[] KoikatuOutfitToken = Encoding.UTF8.GetBytes("【KoiKatuClothes】");
+
+        public static readonly byte[] SexToken = Encoding.UTF8.GetBytes("sex");
+    }
+}
