2eb29ae9fa88cb34b3caae38a5d398146a7f0815
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2021-10-24 12:57:51 +0300

    Improve publish.sh

--- publish.sh
+++ publish.sh
@@ -1,5 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-dotnet publish -r linux-x64 -p:PublishSingleFile=true --no-self-contained -c Release -o bin/publish/linux-x64/
-dotnet publish -r win-x64 -p:PublishSingleFile=true --no-self-contained -c Release -o bin/publish/win-x64/
-cp bin/publish/linux-x64/cardorganizer $HOME/.local/bin/
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+dotnet publish -r linux-x64 -p:PublishSingleFile=true --no-self-contained -c Release -o $SCRIPT_DIR/publish/linux-x64/ $SCRIPT_DIR/CardOrganizer/CardOrganizer.csproj
+#dotnet publish -r win-x64 -p:PublishSingleFile=true --no-self-contained -c Release -o bin/publish/win-x64/
+
+mkdir $HOME/.local/bin
+ln -s $SCRIPT_DIR/publish/linux-x64/cardorganizer $HOME/.local/bin/
