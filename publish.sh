80f8de81252255716cc6e785653534df846676a1
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2023-12-06 20:15:39 +0200

    Remove dotnet version

--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-
-dotnet publish -r linux-x64 -p:PublishSingleFile=true --no-self-contained -c Release -o $SCRIPT_DIR/publish/linux-x64/ $SCRIPT_DIR/CardOrganizer/CardOrganizer.csproj
-#dotnet publish -r win-x64 -p:PublishSingleFile=true --no-self-contained -c Release -o bin/publish/win-x64/
-
-mkdir $HOME/.local/bin
-ln -s $SCRIPT_DIR/publish/linux-x64/cardorganizer $HOME/.local/bin/
